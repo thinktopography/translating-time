08898a66eca69f47640d6dd638643d928f2cfc1b
Greg Kops <greg@thinktopography.com>
2012-06-29 14:22:47 -0400

    Initial Rails checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Translatingtime::Application.config.secret_token = '1d0ef5c2c8378cf6e123e76305141bfd2e115950912413824c7be6bf22cd7edb1a8337c5e8a850bca989672a05f4ae30a28bb14060a73d12b576c15a87197a63'
