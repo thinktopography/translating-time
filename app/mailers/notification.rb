70f62ba8830291bcb66f29fd79946329b28598b1
Greg Kops <greg@thinktopography.com>
2014-04-21 13:44:44 -0400

    fixing password reset

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -2,17 +2,17 @@ class Notification < ActionMailer::Base
 
   def welcome(user)
     @user = user
-    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => user.rfc822, :subject => "Welcome to Translating Time")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => user.rfc822, :subject => "Welcome to Translating Time")
   end
 
   def reset(user)
     @user = user
-    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => user.rfc822, :subject => "Your password has been reset")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => user.rfc822, :subject => "Your password has been reset")
   end
 
   def inquiry(inquiry)
     @inquiry = inquiry
-    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
   end
   
 end
