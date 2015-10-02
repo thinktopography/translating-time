dbab15bb2d0643d232610159f0d1a8c3648e86a9
Greg Kops <greg@thinktopography.com>
2015-10-01 23:01:05 -0400

    disable acts_as_tree and update deployment

--- Capfile
+++ Capfile
@@ -4,5 +4,6 @@ require 'capistrano/bundler'
 require 'capistrano/rails/migrations'
 require 'capistrano/bundler'
 require 'capistrano/passenger'
+load 'deploy/assets'
 
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -2,7 +2,7 @@ class Taxonomy < ActiveRecord::Base
   
   # attr_accessible :parent_id, :name
 
-  acts_as_tree :order => "name"
+  # acts_as_tree :order => "name"
   
   has_and_belongs_to_many :species, :join_table => :classifications
 
