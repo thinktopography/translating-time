4b68b33fc860e0314b8fdcd756ba8f9ae2bc287f
Greg Kops <greg@thinktopography.com>
2015-11-16 09:11:11 -0500

    download files

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -9,15 +9,17 @@ class Admin::ExportsController < Admin::ApplicationController
       @grid[observation.species_id] = {} if @grid[observation.species_id].blank?
       @grid[observation.species_id][observation.event_id] = observation
     end
-    send_data(render_to_string('observations'), :filename => "export-#{Time.now.strftime("%y-%m-%d")}.txt", :type => "application/text", :disposition => "inline")
+    send_data(render_to_string('observations'), :filename => "observation-export-#{Time.now.strftime("%y-%m-%d")}.txt", :type => "application/text", :disposition => "inline") and return
   end
 
   def species
     @species = Species.unscoped.in_model.order("id ASC").all
+    send_data(render_to_string('species'), :filename => "species-export-#{Time.now.strftime("%y-%m-%d")}.txt", :type => "application/text", :disposition => "inline") and return
   end
 
   def events
     @events = Event.unscoped.in_model.order("id ASC").all
+    send_data(render_to_string('events'), :filename => "events-export-#{Time.now.strftime("%y-%m-%d")}.txt", :type => "application/text", :disposition => "inline") and return
   end
 
 end
