0b717bc1cd1a058e1ac9b8ec1990ecc162562b86
Greg Kops <greg@thinktopography.com>
2015-12-08 12:14:22 -0500

    Datasets and compare scripts

--- db/migrate/20151208155401_add_active_flag_to_datasets.rb
+++ db/migrate/20151208155401_add_active_flag_to_datasets.rb
@@ -1,6 +1,6 @@
 class AddActiveFlagToDatasets < ActiveRecord::Migration
   def change
-    add_column :datasets, :is_active, :boolean, :null => false, :default => false
+    # add_column :datasets, :is_active, :boolean, :null => false, :default => false
     model = Model.create
     Dataset.skip_callback("create",:after,:queue_job)
     dataset = Dataset.create(:model_id => model.id, :user_id => 13, :status => 'processed', :is_active => true)
