d1fd0e780d151e410816bc6bfafd7fb64b51ab0b
Greg Kops <greg@thinktopography.com>
2016-11-21 14:00:46 -0500

    fixing email by adding safe sender

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,30 +1,30 @@
 class Notifier < ApplicationMailer
 
   def success(dataset)
-    mail(:from => "Translating Time <greg@thinktopography.com>", :to => dataset.user.email, :subject => 'Your dataset was successfully processed') do |format|
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => dataset.user.email, :subject => 'Your dataset was successfully processed') do |format|
       format.html { render :text => 'Success!' }
     end
   end
 
   def failure(dataset)
-    mail(:from => "Translating Time <greg@thinktopography.com>", :to => dataset.user.email, :subject => 'Your dataset was unable to be processed') do |format|
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => dataset.user.email, :subject => 'Your dataset was unable to be processed') do |format|
       format.html { render :text => 'Failed!' }
     end
   end
 
   def welcome(user)
    @user = user
-   mail(:from => "Translating Time <notifications@translatingtime.net>", :to => user.rfc822, :subject => "Welcome to Translating Time")
+   mail(:from => "Translating Time <translatingtime2@gmail.com>>", :to => user.rfc822, :subject => "Welcome to Translating Time")
  end
 
  def reset(user)
     @user = user
-    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => user.rfc822, :subject => "Your password has been reset")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>>", :to => user.rfc822, :subject => "Your password has been reset")
   end
 
   def inquiry(inquiry)
     @inquiry = inquiry
-    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
   end
-  
+
 end
