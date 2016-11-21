098714cab5dfaa86e90dce9bbd7be8b76b10c009
Greg Kops <greg@thinktopography.com>
2016-11-21 14:02:47 -0500

    fixing typo

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -14,17 +14,17 @@ class Notifier < ApplicationMailer
 
   def welcome(user)
    @user = user
-   mail(:from => "Translating Time <translatingtime2@gmail.com>>", :to => user.rfc822, :subject => "Welcome to Translating Time")
+   mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => user.rfc822, :subject => "Welcome to Translating Time")
  end
 
  def reset(user)
     @user = user
-    mail(:from => "Translating Time <translatingtime2@gmail.com>>", :to => user.rfc822, :subject => "Your password has been reset")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => user.rfc822, :subject => "Your password has been reset")
   end
 
   def inquiry(inquiry)
     @inquiry = inquiry
-    mail(:from => "Translating Time <translatingtime2@gmail.com>>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
+    mail(:from => "Translating Time <translatingtime2@gmail.com>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
   end
 
 end
