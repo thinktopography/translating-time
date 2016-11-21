1c6d5bacc8a56d928f9535b23ccba7d20a4a5785
Greg Kops <greg@thinktopography.com>
2016-11-21 13:55:55 -0500

    move emails to notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -12,4 +12,19 @@ class Notifier < ApplicationMailer
     end
   end
 
+  def welcome(user)
+   @user = user
+   mail(:from => "Translating Time <notifications@translatingtime.net>", :to => user.rfc822, :subject => "Welcome to Translating Time")
+ end
+
+ def reset(user)
+    @user = user
+    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => user.rfc822, :subject => "Your password has been reset")
+  end
+
+  def inquiry(inquiry)
+    @inquiry = inquiry
+    mail(:from => "Translating Time <notifications@translatingtime.net>", :to => 'TranslatingTime2@gmail.com', :subject => "Website Inquiry")
+  end
+  
 end
